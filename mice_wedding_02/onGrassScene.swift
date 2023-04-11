7bddd706b014ce56ac5341df7f645bc5e1cfbf00
Ethan Nguyen <gsthanh@students.cs.ubc.ca>
2023-04-11 06:57:09 -0700

    added collision and animation flash light for exitHole

--- mice_wedding_02/onGrassScene.swift
+++ mice_wedding_02/onGrassScene.swift
@@ -21,9 +21,11 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
 
     var lastTouchLocation: CGPoint? = nil
 
+    // set up the physics category
     struct PhysicsCategory {
-    static let cat: UInt32 = 0x1 << 0
-    static let mouse: UInt32 = 0x1 << 1
+    static let cat: UInt32 = 0x1 << 0 // 1
+    static let mouse: UInt32 = 0x1 << 1 // 2
+    static let exitHole: UInt32 = 0x1 << 2 // 4
     }
 
     // set animation for Nodes
@@ -40,14 +42,20 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
         // animation moving left and right
         let moveLeftRight: SKAction = SKAction.sequence([SKAction.moveBy(x: 20, y: 0, duration: 0.5), SKAction.moveBy(x: -20, y: 0, duration: 0.5)])
         let moveLeftRightContinuously: SKAction = SKAction.repeatForever(moveLeftRight)
-        print("pass moving animation definition")
+        
+        let flashDuration = 4.0 // The duration of each flash
+        let fadeIn = SKAction.fadeAlpha(to: 1.0, duration: flashDuration / 2)
+        let fadeOut = SKAction.fadeAlpha(to: 0.0, duration: flashDuration / 2)
+        let flash = SKAction.sequence([fadeIn, fadeOut])
+        let repeatFlash = SKAction.repeatForever(flash)
+
 
         // set up the animation for nodes and add them to the array list
         for child: SKNode in self.children {
             if let nodeName = child.name, nodeName.hasPrefix("mouse_") { // declare the name of the node in the scene
                 child.run(moveUpDownContinuously)
                 let mouseNode = child as! SKSpriteNode
-                mouseNode.physicsBody = SKPhysicsBody(rectangleOf: mouseNode.size, center: CGPoint(x: 0, y: 0))
+                mouseNode.physicsBody = SKPhysicsBody(rectangleOf: mouseNode.size)
                 mouseNode.physicsBody?.categoryBitMask = PhysicsCategory.mouse // set the category of the node
                 mouseNode.physicsBody?.collisionBitMask = PhysicsCategory.cat // set the collision of the node
                 mouseNode.physicsBody?.contactTestBitMask = PhysicsCategory.cat // set the contact of the node
@@ -58,7 +66,7 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
             } else if child.name == "cat" { // declare the name of the node in the scene
                 child.run(moveLeftRightContinuously)
                 let catNode = child as! SKSpriteNode
-                catNode.physicsBody = SKPhysicsBody(rectangleOf: catNode.size, center: CGPoint(x: 0, y: 0))
+                catNode.physicsBody = SKPhysicsBody(rectangleOf: catNode.size)
                 catNode.physicsBody?.categoryBitMask = PhysicsCategory.cat
                 catNode.physicsBody?.collisionBitMask = PhysicsCategory.mouse
                 catNode.physicsBody?.contactTestBitMask = PhysicsCategory.mouse
@@ -66,8 +74,16 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
                 catNode.physicsBody?.allowsRotation = false
 
                 cats.append(catNode)
-            } 
-            print("didMove: successfully added cats & mice")
+            } else if child.name == "exitHole" {
+                child.run(repeatFlash)
+                let exitHoleNode: SKSpriteNode = child as! SKSpriteNode
+                exitHoleNode.physicsBody = SKPhysicsBody(rectangleOf: exitHoleNode.size)
+                exitHoleNode.physicsBody?.categoryBitMask = PhysicsCategory.exitHole // set the category of the node
+                exitHoleNode.physicsBody?.collisionBitMask = PhysicsCategory.mouse // set the collision of the node
+                exitHoleNode.physicsBody?.contactTestBitMask = PhysicsCategory.mouse // set the contact of the node
+                exitHoleNode.physicsBody?.affectedByGravity = false // set the gravity of the node
+            }
+            
         }
     }
 
@@ -122,13 +138,13 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
         currentMouse.physicsBody?.velocity = velocity
         
         // update mice moving after the leading mouse
-        let distanceBetweenMice: CGFloat = 25
+        let distanceBetweenMice: CGFloat = 50
         if let miceCount = mice?.count, miceCount > 1 {
             for i in (1..<miceCount).reversed() {
                 let mouse: SKSpriteNode = mice![i]
                 let previousMouse: SKSpriteNode = mice![i - 1]
                 let newPosition: CGPoint = CGPoint(x: previousMouse.position.x - distanceBetweenMice, y: previousMouse.position.y)
-                let moveAction: SKAction = SKAction.move(to: newPosition, duration: 0.3)
+                let moveAction: SKAction = SKAction.move(to: newPosition, duration: 0.2)
                 mouse.run(moveAction)
                 mouse.zPosition = previousMouse.zPosition + 1 // Ensure nodes are layered properly
             }
@@ -175,12 +191,14 @@ public class onGrassScene: SKScene, SKPhysicsContactDelegate {
 
         if firstBody.categoryBitMask == PhysicsCategory.cat && secondBody.categoryBitMask == PhysicsCategory.mouse {
             upLevel(true)
+        } else if firstBody.categoryBitMask == PhysicsCategory.mouse && secondBody.categoryBitMask == PhysicsCategory.exitHole {
+            upLevel(true)
         }
     }
 
     // handle the end of the game
     fileprivate func upLevel(_ win: Bool) {
-        let transition = SKTransition.flipHorizontal(withDuration: 1.0)
+        let transition = SKTransition.fade(withDuration: 1.0)
         let blankScene = SKScene(size: view!.bounds.size)
         view?.presentScene(blankScene, transition: transition)
     }
