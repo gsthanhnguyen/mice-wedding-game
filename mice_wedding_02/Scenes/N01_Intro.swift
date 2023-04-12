8199ed55a03a1287e426f7167ad0e791736a6627
Ethan Nguyen <gsthanh@students.cs.ubc.ca>
2023-04-12 06:44:24 -0700

    Done N01

--- mice_wedding_02/Scenes/N01_Intro.swift
+++ mice_wedding_02/Scenes/N01_Intro.swift
@@ -11,6 +11,8 @@ import SpriteKit
 public class N01_Intro: SKScene, SKPhysicsContactDelegate {
 
     var buttonScene_01: SKSpriteNode!
+    var N01_cat: SKSpriteNode!
+    var N01_mice: SKSpriteNode!
     var sound: SKAudioNode!
     
     override public func didMove(to view: SKView) {     
@@ -19,12 +21,24 @@ public class N01_Intro: SKScene, SKPhysicsContactDelegate {
         let fadeOut = SKAction.fadeAlpha(to: 0.5, duration: 1)
         let flash = SKAction.sequence([fadeIn, fadeOut])
         let repeatFlash = SKAction.repeatForever(flash) // call this action to make the node fade in and out continuously
+        // animation moving up and down
+        let moveUpDown: SKAction = SKAction.sequence([SKAction.moveBy(x: 0, y: 20, duration: 0.5), SKAction.moveBy(x: 0, y: -20, duration: 0.5)])
+        let moveUpDownContinuously: SKAction = SKAction.repeatForever(moveUpDown)
+        
 
         // Create button node
-        buttonScene_01 = childNode(withName: "buttonScene") as? SKSpriteNode
+        buttonScene_01 = childNode(withName: "N01_button") as? SKSpriteNode
         buttonScene_01.run(repeatFlash)
+
+        N01_cat = childNode(withName: "N01_cat") as? SKSpriteNode
+        N01_cat.run(moveUpDownContinuously)
+
+        N01_mice = childNode(withName: "N01_mice") as? SKSpriteNode
+        N01_mice.run(moveUpDownContinuously)
         
-        self.run(SKAction.playSoundFileNamed("sound.mp3", waitForCompletion: false))
+        // Sound credit: Music from #Uppbeat (free for Creators!):https://uppbeat.io/t/soundroll/the-incident
+        // License code: ZZ1XRJ9UWQ5JTVUD
+        self.run(SKAction.playSoundFileNamed("Sound.mp3", waitForCompletion: false))
 
     }
     
